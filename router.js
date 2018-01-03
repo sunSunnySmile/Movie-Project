9f1606a0a81b966ff770d4fe44dd3295ac5b85ee
zouting <972721300@qq.com>
2018-01-03 10:24:58 +0800

    home页面的模板完成

--- router.js
+++ router.js
@@ -6,4 +6,22 @@ routerApp.config(['$stateProvider', function(stateProvider) {
         templateUrl: "./home/home.html"
             // controller:""
     })
+    stateProvider.state({
+        name: "comming_soon",
+        url: "/comming_soon",
+        templateUrl: "./comming_soon/comming_soon.html"
+            // controller:""
+    })
+    stateProvider.state({
+        name: "top250",
+        url: "/top250",
+        templateUrl: "./top250/top250.html"
+            // controller:""
+    })
+    stateProvider.state({
+        name: "in_theaters",
+        url: "/in_theaters",
+        templateUrl: "./in_theaters/in_theaters.html"
+            // controller:""
+    })
 }])
